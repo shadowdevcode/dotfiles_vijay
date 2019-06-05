f4eb3377e0cb91dcc6abfa37d9a0302026bf8bda
Vijay Sehgal <vijaysehgal@Vijays-MacBook-Pro.local>
2019-06-05 23:52:35 +0530

    Added dotfiles for setting new machine

---
+++ brew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install skype
+brew cask install vlc
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
+
