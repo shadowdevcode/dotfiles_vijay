a81a93e5dd41f5dc91e81b4c52fcfeb37e7ffe7c
shadowdevcode <vijay.b.sehgal@gmail.com>
2019-07-09 20:47:20 +0530

    Added softwares to install

--- brew.sh
+++ brew.sh
@@ -3,12 +3,18 @@
 # Install Brew Packages
 brew install python
 brew install tree
+brew install node
+brew install speedtest-cli
 
 # Install MacOS Applications
 brew cask install google-chrome
 brew cask install sublime-text
 brew cask install skype
 brew cask install vlc
+brew cask install postman
+brew cask install eatagrip
+brew cask install firefox
+brew cask install slack
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
